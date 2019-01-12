285108d06652d453014d30aab2f00c61b4122ef8
Jean-Philippe Evrard <jean-philippe@evrard.me>
2019-01-12 09:58:45 +0100

    Add Gerrit pubkey trust and checking

--- bump.sh
+++ bump.sh
@@ -5,6 +5,20 @@ set -o xtrace
 
 osa_url="https://github.com/openstack/openstack-ansible.git"
 
+# Add openstack's gerrit ssh public key
+# Known public key
+gerrit_pubkey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfsIj/jqpI+2CFdjCL6kOiqdORWvxQ2sQbCzSzzmLXic8yVhCCbwarkvEpfUOHG4eyB0vqVZfMffxf0Yy3qjURrsroBCiuJ8GdiAcGdfYwHNfBI0cR6kydBZL537YDasIk0Z3ILzhwf7474LmkVzS7V2tMTb4ZiBS/jUeiHsVp88FZhIBkyhlb/awAGcUxT5U4QBXCAmerYXeB47FPuz9JFOVyF08LzH9JRe9tfXtqaCNhlSdRe/2pPRvn2EIhn5uHWwATACG9MBdrK8xv8LqPOik2w1JkgLWyBj11vDd5I3IjrmREGw8dqImqp0r6MD8rxqADlc1elfDIXYsy+TVH"
+# Found public key (for comparison purposes, avoiding mitm)
+gerrit_found_pubkey=$(ssh-keyscan -p 29418 review.openstack.org 2>/dev/null | awk '/[review.openstack.org]/{gsub(/\[review.openstack.org\]:29418 /,"",$0); print $0;}')
+
+if [[ ${gerrit_pubkey} != ${gerrit_found_pubkey} ]]; then
+    echo "Alert! MITM detected"
+    exit 1
+else
+    echo "Good key found, can ssh in"
+    echo ${gerrit_pubkey} >> ~/.ssh/known_hosts
+fi
+
 # Backup gitconfig if it exists already
 if [[ -f ~/.gitconfig ]]; then
     cp ~/.gitconfig ~/.gitconfig.old
@@ -44,7 +58,7 @@ pushd "osa-$branch"
     git add .
     git commit -F "../commitmsg-$branch"
     osa releases check_pins
-    if [[ $(expr `date +'%V'` % 2) -eq 0 ]]; then
+    if [[ $(( $(date +'%V') % 2)) -eq 0 ]]; then
         echo "Even week number, bumping!"
         git review -f -t bump_osa
     else
